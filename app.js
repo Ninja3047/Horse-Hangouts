9bd59e4e93f87afe27a1cd96c2f5e471beef9573
Max <themaxaxis@gmail.com>
2014-12-22 16:47:39 -0500

    oops still

--- app.js
+++ app.js
@@ -180,8 +180,8 @@ bot.on('message', function(from, message) {
       if (message !== undefined) {
         if (message.length > 1) {
           var sections = message.split("|");
-          sections[0] = sections[0].replace(/[^]/gi, '');
-          if (sections.length == 2 && sections[0] !== "") {
+          sections[0] = sections[0].replace(/[^\s]/gi, '');
+          if (sections.length == 2) {
             if (learnThings.terms.indexOf(sections[0]) == -1 && cmds.commands.indexOf(sections[0]) == -1 && blacklist.indexOf(sections[0]) == -1) {
               learnThings.terms.push(sections[0]);
               learnThings.definitions.push(sections[1]);
